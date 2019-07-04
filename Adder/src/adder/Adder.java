6e76c338112dbc22870c1f1994f35cc16e7a2f9f
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2019-07-03 23:39:28 -0500

    Add files via upload

---
+++ Adder/src/adder/Adder.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adder;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Adder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int main(String[] args) {
+        
+        ArrayList <Integer> adder = new ArrayList();
+        Scanner s = new Scanner(System.in);
+        int suma=0;
+        for (int i = 0; i < adder.size(); i++){
+            for (int k = 0;k <= adder.get();k++){
+                for (int j = 0; j = adder.get(); j++){
+                    if (j >= j+1){
+                        suma = suma+j++;
+                        return suma;
+                        if (suma == j++){
+                            return k++;
+                        }
+                    }
+                }    
+            }
+            return i++;
+        }
+    }
+    
+}
